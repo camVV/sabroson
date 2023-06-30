17220206b51031cec1f1bb5061de05af8aec9eb5
unknown <camilovargas837@gmail.com>
2023-06-30 15:03:47 +0200

    actulizado

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import imagentest from '../images/grid3imagen.jpg';
+import { NavLink } from "react-router-dom";
+import imagencontacto from '../images/fron4.jpg';
+import imgagenlogo2 from '../images/logo.png';
 import { BsTelephone } from "react-icons/bs";
 import {IoLocationSharp} from "react-icons/io5";
 import {BsTelephoneFill} from "react-icons/bs";
@@ -6,21 +10,23 @@ import {HiOutlineMail} from "react-icons/hi";
 
 export const Contacto = () => {
   return (
-    <div className='layout-2'>
+
+<div class="card cardContacto">
       
-      <div className='imgcontacto'>
-      <h2>Contacto</h2>        
-      <span> <BsTelephone/></span>
-      </div>
-      <div className='container-contact'>
-      <div className='location-contacto'>
-       
-       <div className='text-location'><span className='log-contact'><IoLocationSharp/></span><span className='text-contacts'>Calle Cromo 15 Poligono Calonge. Sevilla</span></div>
-        <div className='text-mail'><span className='log-contact'><HiOutlineMail/></span ><a className='text-contacts' id='email' href='mailto:sabrosonsevilla@gmail.com'>sabrosonsevilla@gmail.com</a></div>
-        <div className='tel-text'><span className='log-contact'><BsTelephoneFill/></span><a className='text-contacts' id='tel' href='tel:+34610071324'>610 07 13 24</a></div>
-       </div>  
-    </div>
-    <div className='map'><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12677.105963788068!2d-5.9563329!3d37.4069392!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126985f0e5a8f3%3A0x3122be73e9938256!2sEL%20SABROSON%20SEVILLA!5e0!3m2!1ses!2ses!4v1687768241764!5m2!1ses!2ses" ></iframe></div>
+<div class="ladoIzq ladoizqContacto"><img src={imagencontacto}></img> </div>
+    <div class="ladoDer">
+      <h2 class="titulo">Contacto</h2>
+      <div class="cuerpo cuerpo-texto">
+        <p>
+          <span className='bold-eventos-card'>Email:</span> sabrosonsevilla@gmail.com</p>
+       <p> 
+        <span className='bold-eventos-card'>Telefono:</span> 954 55 55 55</p>
+        <p>
+        <span className='bold-eventos-card'>Localización:</span> Calle Cromo 15</p>
+      </div>  
+      <div class="centrahrz"><NavLink to="https://goo.gl/maps/TqNJP56JzCtZEYxE7" target='_blanc'><span className='btn'> Ver en el Mapa</span></NavLink></div>
+  </div>
+   
     </div>
   )
 }
